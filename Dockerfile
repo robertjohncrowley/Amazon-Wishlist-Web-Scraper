ef7f9263c1bbbc23a88be75d2db12608b1c16d26
Rob Crowley <robert.crowley1995@gmail.com>
2021-02-09 21:02:09 +0000

    Scraping Amazon Wishlist to HTML file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM python:3
 ADD wishlist_scrape.py /
+COPY requirements.txt ./
+RUN pip3 install --no-cache-die -r requirements.txt
+COPY . .
 CMD [ "python", "./wishlist_scrape.py" ]
--- wishlist_scrape.py
+++ wishlist_scrape.py
@@ -1,1 +1,13 @@
-print("Hello, Wishlist")
+import scrapy
+
+class WishlistSpider(scrapy.Spider):
+    name = "wishlist_spider"
+    allowed_domains = ["amazon.co.uk"]
+    start_urls = [
+        "https://www.amazon.co.uk/hz/wishlist/ls/107HD31LKJTN3?ref_=wl_share"
+    ]
+
+    def parse(self, response):
+        filename = "wishlist.html"
+        with open(filename, 'wb') as f:
+            f.write(response.body)
